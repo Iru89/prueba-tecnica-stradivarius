479a998cd628503e44e399a61cb802bb27526723
oriol raventos <o.raventos.89@gmail.com>
2020-12-23 23:35:47 +0100

    canvi en la key

--- src/components/Main/Grid.jsx
+++ src/components/Main/Grid.jsx
@@ -11,14 +11,14 @@ const Grid = ({items}) => {
             <div className="card">
                 <div className="card-text">{items[0].text}</div>
                 {items[0] && items[0].links && items[0].links.length > 0 
-                    ? items[0].links.map(link => <div className="card-link"><a href={link.url} target="_blank" rel="noopener noreferrer">{link.text}</a></div>) 
+                    ? items[0].links.map((link, index) => <div key={`${link.text}-${index}`} className="card-link"><a href={link.url} target="_blank" rel="noopener noreferrer">{link.text}</a></div>) 
                     : null }
             </div>
             <div  className="card">
                 <div className="card-text">{items[1].text}</div>
                 <div>
                 {items[1] && items[1].links && items[1].links.length > 1 
-                    ? items[1].links.map(link => <div key={link.text} className="card-link"><a href={link.url} target="_blank" rel="noopener noreferrer">{link.text}</a></div>) 
+                    ? items[1].links.map((link, index) => <div key={`${link.text}-${index}`} className="card-link"><a href={link.url} target="_blank" rel="noopener noreferrer">{link.text}</a></div>) 
                     : null }
                 </div>
             </div>
