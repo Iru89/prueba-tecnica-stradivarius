c54686c30b5521cf9ff02d00fef8022ad179acf7
oriol raventos <o.raventos.89@gmail.com>
2020-12-23 20:42:40 +0100

    afegides url al jsonFAKE

--- src/utils/core.js
+++ src/utils/core.js
@@ -15,14 +15,26 @@ export const fetchFAKE = () => {
                         textColor: "red",
                         title: "COLECCIÓN FIESTA",
                         percentatge: [30, 40, 50],
-                        buttons: [{ text: "COMPRAR", url: "" }],
+                        buttons: [
+                            {
+                                text: "COMPRAR",
+                                url:
+                                    "https://www.stradivarius.com/es/mujer/special-prices/fiesta--30%25-c1020329006.html"
+                            }
+                        ],
                         urlImage:
                             "https://static.e-stradivarius.net/5/static2/homes/2020_w51/img/mkt-w/1920/2400_fiesta.jpg?t=20201222025002"
                     },
                     {
                         textColor: "white",
                         title: "NEW IN",
-                        buttons: [{ text: "Ver todo", url: "" }],
+                        buttons: [
+                            {
+                                text: "Ver todo",
+                                url:
+                                    "https://www.stradivarius.com/es/mujer/nuevo-c1020161505.html"
+                            }
+                        ],
                         urlImage:
                             "https://static.e-stradivarius.net/5/static2/homes/2020_w51/img/mkt-w/1920/2400_newin.jpg?t=20201222025002"
                     }
@@ -32,14 +44,26 @@ export const fetchFAKE = () => {
                 type: "image-1",
                 textColor: "white",
                 title: "ABRIGOS",
-                buttons: [{ text: "Ver todo", url: "" }],
+                buttons: [
+                    {
+                        text: "Ver todo",
+                        url:
+                            "https://www.stradivarius.com/es/mujer/ropa/compra-por-producto/abrigos/ver-todo-c1020170027.html"
+                    }
+                ],
                 urlImage:
                     "https://static.e-stradivarius.net/5/static2/homes/2020_w51/img/mkt-w/1920/2400_abrigos.jpg?t=20201222025002"
             },
             {
                 type: "image-2",
                 textColor: "white",
-                buttons: [{ text: "Ver todo", url: "" }],
+                buttons: [
+                    {
+                        text: "Ver todo",
+                        url:
+                            "https://www.stradivarius.com/es/mujer/ropa/compra-por-producto/str-c1020210502.html"
+                    }
+                ],
                 urlImage:
                     "https://static.e-stradivarius.net/5/static2/homes/2020_w51/img/mkt-w/1920/2400_str.jpg?t=20201223023502",
                 urlLogo:
@@ -50,8 +74,16 @@ export const fetchFAKE = () => {
                 textColor: "white",
                 title: "EFFORTLESSLY COOL",
                 buttons: [
-                    { text: "PUNTO", url: "" },
-                    { text: "JEANS", url: "" }
+                    {
+                        text: "PUNTO",
+                        url:
+                            "https://www.stradivarius.com/es/mujer/ropa/compra-por-producto/punto/ver-todo-c1718564.html"
+                    },
+                    {
+                        text: "JEANS",
+                        url:
+                            "https://www.stradivarius.com/es/mujer/ropa/compra-por-producto/jeans/ver-todo-c1718557.html"
+                    }
                 ],
                 urlImage:
                     "https://static.e-stradivarius.net/5/static2/homes/2020_w51/img/mkt-w/1920/2400_punto_jeans.jpg?t=20201223023502"
@@ -62,14 +94,26 @@ export const fetchFAKE = () => {
                     {
                         textColor: "white",
                         title: "ZAPATOS",
-                        buttons: [{ text: "VER TODO", url: "" }],
+                        buttons: [
+                            {
+                                text: "VER TODO",
+                                url:
+                                    "https://www.stradivarius.com/es/mujer/zapatos/compra-por-producto/todos/todos-c1020178528.html"
+                            }
+                        ],
                         urlImage:
                             "https://static.e-stradivarius.net/5/static2/homes/2020_w51/img/mkt-w/1920/2400_zapatos.jpg?t=20201223023502"
                     },
                     {
                         textColor: "white",
                         title: "ACCESORIOS",
-                        buttons: [{ text: "VER TODO", url: "" }],
+                        buttons: [
+                            {
+                                text: "VER TODO",
+                                url:
+                                    "https://www.stradivarius.com/es/mujer/accesorios/compra-por-producto/nuevo/ver-todo-c1020303541.html"
+                            }
+                        ],
                         urlImage:
                             "https://static.e-stradivarius.net/5/static2/homes/2020_w51/img/mkt-w/1920/2400_accesorios.jpg?t=20201223023502"
                     }
@@ -84,8 +128,16 @@ export const fetchFAKE = () => {
                 title: "The holiday issue",
                 textColor: "white",
                 buttons: [
-                    { text: "VER EDITORIAL", url: "" },
-                    { text: "GIFT GUIDE", url: "" }
+                    {
+                        text: "VER EDITORIAL",
+                        url:
+                            "https://www.stradivarius.com/es/mujer/the-holiday-issue/editorial-c1020323502.html"
+                    },
+                    {
+                        text: "GIFT GUIDE",
+                        url:
+                            "https://www.stradivarius.com/es/mujer/the-holiday-issue/gift-guide/ver-todo-c1020265152.html"
+                    }
                 ]
             },
             {
@@ -109,7 +161,13 @@ export const fetchFAKE = () => {
                         urlImg:
                             "https://static.e-stradivarius.net/5/static2/homes/2020_w51/img/mkt-w/1920/Stradishoppers/2400_stradishoppers_2.jpg?t=20201223023502",
                         text: "DESCUBRE NUESTROS NUEVOS VÍDEOS COMPRABLES",
-                        links: [{ text: "IR AL CANAL", url: "" }]
+                        links: [
+                            {
+                                text: "IR AL CANAL",
+                                url:
+                                    "https://www.stradivarius.com/es/mujer/nuevo-c1020161505.html?eCommTV=true"
+                            }
+                        ]
                     },
                     {
                         urlImg:
@@ -117,8 +175,16 @@ export const fetchFAKE = () => {
                         text:
                             "NO TE PIERDAS LAS ACTUALIZACIONES SEMANALES EN STREAMING DE NUESTRAS STRADISHOPPERS. VE I COMPRA ONLINE LO MÁS NUEVO DE NUESTRA TIENDA",
                         links: [
-                            { text: "@ANDREASTRADISHOPPER", url: "" },
-                            { text: "@ANASTRADISHOPPER", url: "" }
+                            {
+                                text: "@ANDREASTRADISHOPPER",
+                                url:
+                                    "https://www.stradivarius.com/es/mujer/nuevo-c1020161505.html?eCommTV=true"
+                            },
+                            {
+                                text: "@ANASTRADISHOPPER",
+                                url:
+                                    "https://www.stradivarius.com/es/mujer/nuevo-c1020161505.html?eCommTV=true"
+                            }
                         ]
                     }
                 ]
