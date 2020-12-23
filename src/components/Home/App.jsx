c2bfeca3bff5d6c29b617dda6fe095c16e63b659
oriol raventos <o.raventos.89@gmail.com>
2020-12-23 17:25:20 +0100

    eliminat el header

--- src/components/Home/App.jsx
+++ src/components/Home/App.jsx
@@ -1,13 +1,9 @@
-import { Header } from "../Header/Header"
 import { Main } from "../Main/Main"
 import "./css/App.css";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <Header />               
-            </header>
             <Main />
         </div>
     );
